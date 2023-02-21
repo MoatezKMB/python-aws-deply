bdfff87a6fee0569285e9c3fd1f34cc5eca6d19d
KARABAGHLI <mouatez-bellah.karabaghli@capgemini.com>
2023-02-21 11:30:44 +0100

    try a ver

--- application.py
+++ application.py
@@ -1,6 +1,15 @@
-from flask import Flask
+'''from flask import Flask
 application= Flask(__name__)
 @application.route('/')
 
 def hello_mouatez():
     return 'hello KARABAGHLI Mouatez Bellah'
+'''
+from flask import Flask, request,render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/vmd_timestamp')
+def vmd_timestamp():
+    return render_template('vmd_timestamp.html')
