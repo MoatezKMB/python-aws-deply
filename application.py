0b4ff03651555f231ebea9b30ea28ccf7a29694d
KARABAGHLI <mouatez-bellah.karabaghli@capgemini.com>
2023-02-21 14:42:31 +0100

    back to V1

--- application.py
+++ application.py
@@ -1,28 +1,9 @@
-'''from flask import Flask
+from flask import Flask
 application= Flask(__name__)
 @application.route('/')
 
 def hello_mouatez():
     return 'hello KARABAGHLI Mouatez Bellah'
 
-hello_mouatez()'''
-'''
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    '''
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-   return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+hello_mouatez() 
+ 
