0e53a32d839546c2257f99fa60ff5697b558e475
KARABAGHLI <mouatez-bellah.karabaghli@capgemini.com>
2023-02-21 11:40:35 +0100

    add button

--- application.py
+++ application.py
@@ -1,8 +1,19 @@
-from flask import Flask
+'''from flask import Flask
 application= Flask(__name__)
 @application.route('/')
 
 def hello_mouatez():
     return 'hello KARABAGHLI Mouatez Bellah'
 
-hello_mouatez()
+hello_mouatez()'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
